9c814edab2eee63b1d28ec19d163a5b63880f75b
JustinG <justin.gestiere@gmail.com>
2025-12-17 15:30:25 +0100

    test route

--- app.js
+++ app.js
@@ -22,6 +22,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// test
+app.get('/test', (req, res) => {
+  res.send('TEST OK');
+});
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
